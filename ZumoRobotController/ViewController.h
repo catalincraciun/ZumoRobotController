6ced38f19424450dd4c04e897a5eb5116b7c49a5
Catalin Craciun <catalincraciun7@yahoo.com>
2014-09-29 20:41:01 +0300

    Initial Commit

---
+++ ZumoRobotController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZumoRobotController
+//
+//  Created by Cătălin Crăciun on 29/09/14.
+//  Copyright (c) 2014 Cătălin Crăciun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZumoRobotController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZumoRobotController
+//
+//  Created by Cătălin Crăciun on 29/09/14.
+//  Copyright (c) 2014 Cătălin Crăciun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
